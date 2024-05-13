b692ebc072179da31f44201ceec7118b477bd614
Daniel Prieto <prietogaviriadaniel@gmail.com>
2024-05-13 11:22:28 +0200

    feat: adding react hook form to better gif search

--- app/components/SeacrhForm/SearchForm.js
+++ app/components/SeacrhForm/SearchForm.js
@@ -3,78 +3,95 @@
 import { memo, useCallback } from 'react'
 import { useRouter } from 'next/navigation'
 import useForm from './useForm'
+import { useForm as useFormReact } from 'react-hook-form'
 import Search from '../icons/Search'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import {
   Select,
   SelectContent,
+  SelectGroup,
   SelectItem,
+  SelectLabel,
   SelectTrigger,
   SelectValue
 } from '@/components/ui/select'
+import { Form, FormControl, FormField, FormItem } from '@/components/ui/form'
 
-const RATINGS = ['g', 'pg', 'pg-13', 'r']
-
-function SearchForm({ initialKeyword = '', initialRating = RATINGS[0] }) {
+function SearchForm({ initialKeyword = '', initialRating = 'g' }) {
   const router = useRouter()
 
-  const { keyword, rating, times, updateKeyword, updateRating } = useForm({
+  const { keyword } = useForm({
     initialKeyword,
     initialRating
   })
 
-  const handleChange = (e) => {
-    updateKeyword(e.target.value)
-  }
+  const form = useFormReact()
 
-  const handleSubmit = useCallback(
-    (e) => {
-      e.preventDefault()
+  const onSubmit = useCallback(
+    ({ keyword, rating = 'g' }) => {
       router.push(`/Search/${keyword}/${rating}`)
     },
-    [router, keyword, rating]
+    [router]
   )
 
-  const handleChangeRating = (e) => {
-    updateRating(e.target.value)
-  }
-
   return (
-    <form
-      className='w-full flex flex-col items-center justify-center gap-4 2xl:gap-10'
-      onSubmit={handleSubmit}
-    >
-      <div className='w-full flex items-center justify-center relative'>
-        <Input
-          onChange={handleChange}
-          type='text'
-          value={keyword}
-          placeholder='Buscar...'
-          required
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className='w-full flex flex-col items-center justify-center gap-4 2xl:gap-10'
+      >
+        <FormField
+          control={form.control}
+          name='keyword'
+          render={({ field }) => (
+            <FormItem className='w-full'>
+              <FormControl>
+                <div className='w-full flex items-center justify-center relative'>
+                  <Input
+                    value={keyword}
+                    placeholder='Buscar...'
+                    required
+                    {...field}
+                  />
+                  <Search className='w-4 h-4 text-neutral-400 absolute right-4 lg:w-5 lg:h-5 lg:right-6 2xl:right-8' />
+                </div>
+              </FormControl>
+            </FormItem>
+          )}
         />
-        <Search className='w-4 h-4 text-neutral-400 absolute right-4 lg:w-5 lg:h-5 lg:right-6 2xl:right-8' />
-      </div>
-      <aside className='flex items-center justify-center gap-4 2xl:gap-8'>
-        <Select
-          className='block w-24 h-8 px-4 border border-neutral-700 bg-primary-black text-primary-white placeholder-gray-400 font-medium text-sm rounded-lg lg:w-32 lg:h-11 lg:text-lg 2xl:px-6 2xl:w-48 2xl:h-16 2xl:text-2xl 2xl:rounded-xl'
-          onChange={handleChangeRating}
-          value={rating}
-        >
-          <SelectTrigger className='w-[86px] lg:w-[102px] 2xl:w-[112px]'>
-            <SelectValue placeholder='Rating' />
-          </SelectTrigger>
-          <SelectContent>
-            {RATINGS.map((rating) => (
-              <SelectItem key={rating} value={rating}>
-                {rating}
-              </SelectItem>
-            ))}
-          </SelectContent>
-        </Select>
-        <Button type='submit'>Buscar</Button>
-      </aside>
-    </form>
+        <div className='w-full flex items-center justify-center gap-4 2xl:gap-10'>
+          <FormField
+            control={form.control}
+            name='rating'
+            render={({ field }) => (
+              <FormItem>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger className='w-[86px] lg:w-[102px] 2xl:w-[112px]'>
+                      <SelectValue placeholder='g' />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectLabel>Select a Rating</SelectLabel>
+                      <SelectItem value='g'>g</SelectItem>
+                      <SelectItem value='pg'>pg</SelectItem>
+                      <SelectItem value='pg-13'>pg-13</SelectItem>
+                      <SelectItem value='r'>r</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </FormItem>
+            )}
+          />
+          <Button type='submit'>Submit</Button>
+        </div>
+      </form>
+    </Form>
   )
 }
 
