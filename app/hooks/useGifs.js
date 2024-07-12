8dcd62474de4b6a00610fb91dc1a3b46f8685287
Daniel Prieto <prietogaviriadaniel@gmail.com>
2024-07-12 08:51:05 +0200

    fix: localstorage from server components

--- app/hooks/useGifs.js
+++ app/hooks/useGifs.js
@@ -9,20 +9,25 @@ const INITIAL_PAGE = 0
 export function useGifs({ keyword, rating } = { keyword: null }) {
   const [loading, setLoading] = useState(false)
   const [loadingNextPage, setLoadingNextPage] = useState(false)
+  const [keywordToUse, setKeywordToUse] = useState(keyword || 'random')
 
   const [page, setPage] = useState(INITIAL_PAGE)
   const { gifs, setGifs } = useContext(GifsContext)
 
-  const keywordToUse =
-    localStorage.getItem('lastKeyword') || keyword || 'random'
-  // const keywordToUse = keyword || 'random'
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const storedKeyword =
+        window.localStorage.getItem('lastKeyword') || keyword || 'random'
+      if (storedKeyword && !keyword) setKeywordToUse(storedKeyword)
+    }
+  }, [keyword])
 
   useEffect(() => {
     setLoading(true)
     getGifs({ keyword: keywordToUse, rating }).then((gifs) => {
       setGifs(gifs)
       setLoading(false)
-      if (keyword) localStorage.setItem('lastKeyword', keyword)
+      if (keyword) window.localStorage.setItem('lastKeyword', keyword)
     })
   }, [keyword, keywordToUse, setGifs, rating])
 
